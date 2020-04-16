d722fa635a33b7d2fe3c120af54d0dd1660b9c53
Felipe <felipezb@live.com>
2020-04-16 18:00:01 +0300

    start - dynamic create elements

--- main.js
+++ main.js
@@ -1,60 +1,110 @@
-
-var buildPaymentForm = function (cards, paymentForm, methodContainer, method) {
-    var cardDiv = document.createElement("div");
-    cardDiv.className = "form-container";
-    cardDiv.id = "selectedMethod";
-    
-    var methodForm = document.createElement("div");
-    methodForm.id = method.id;
-    methodForm.innerText = method.getAttribute('data-customField');
-
-
-    var closeMethodBtn = document.createElement("button");
-    closeMethodBtn.id = "closeMethodBtn"
-    closeMethodBtn.innerText = "Close";
-    closeMethodBtn.addEventListener("click", function () {
-        if (paymentForm.firstElementChild != null) {
-            methodContainer.classList = "flex-container";
-            paymentForm.removeChild(paymentForm.firstChild);
-
-            for (var i = 0; i < cards.length; i++) {
-                cards[i].classList.remove("card-small");
-            }
-        }
-    });
-
-    cardDiv.appendChild(methodForm);
-    cardDiv.appendChild(closeMethodBtn);
-
-    paymentForm.appendChild(cardDiv);
-};
-
-
 window.addEventListener("load", function () {
     console.log("Document loaded!")
     var cards = document.querySelectorAll(".card");
-    var paymentForm = document.getElementById("cashierPaymentForm");
+    var cashierFormSection = document.getElementById("cashierPaymentForm");
     var methodContainer = document.getElementById("methodsContainer");
-    
+
     for (var i = 0; i < cards.length; i++) {
         cards[i].addEventListener('click', function () {
 
-            methodContainer.classList = "scrolling-wrapper";    
+            methodContainer.classList = "scrolling-wrapper";
 
-            if (paymentForm.firstElementChild != null) {
-                paymentForm.removeChild(paymentForm.firstChild);
-            }
-            buildPaymentForm(cards, paymentForm, methodContainer,this);
+            cashierFormSection.removeChild(cashierFormSection.firstChild);
             
+            buildPaymentForm(cards, cashierFormSection, this);
+
             for (var i = 0; i < cards.length; i++) {
                 cards[i].classList.add("card-small");
+                cards[i].style.display = 'flex';
             }
+
+            this.style.display = 'none';
 
         }, false);
     }
-
-    
-
-
-
 });
+
+
+function buildPaymentForm(cards, cashierFormSection, method) {
+    var methodData = getMethodData(method);
+
+    //<div id="selectedMethod" class="method-container">
+    var selectedMethod = document.createElement("div");
+    selectedMethod.className = "method-container";
+    selectedMethod.id = methodData.id;
+
+    //<form id="cashierForm" class="form-wrapper">
+    var form = document.createElement("form");
+    form.id = "cashierForm";
+    form.classList = "form-wrapper";
+    form.appendChild(buildLogoAmountRow(methodData));
+
+    selectedMethod.appendChild(form);
+    cashierFormSection.appendChild(selectedMethod);
+};
+
+function buildLogoAmountRow(method) {
+    //<div class="logo-amount-row">
+    var logoAmountRow = document.createElement("div");
+    logoAmountRow.classList = "logo-amount-row";
+
+    logoAmountRow.appendChild(buildLogo(method));
+    logoAmountRow.appendChild(buildAmount(method));
+
+    return logoAmountRow;
+}
+
+function buildLogo(method) {
+     //<div class="form-logo">
+     var divLogo = document.createElement("div");
+     divLogo.classList = "form-logo";
+ 
+     //<img src="img/bigwallet.svg">
+     var logo = document.createElement("img");
+     logo.src = method.logo;
+ 
+     //<small>10 EUR - EUR</small>
+     var amountRangeText = document.createElement("small");
+     amountRangeText.innerHTML = method.amountrange;
+ 
+     divLogo.appendChild(logo);
+     divLogo.appendChild(amountRangeText);
+
+     return divLogo;
+}
+
+function buildAmount(method) {
+    //<div class="amount-container">
+        //<div class="predefined-amount-row">
+            //<span>Amount: </span>
+            //<label class="radio-container">
+                //<input id="amount100" name="amount" type="radio" value="100" checked="checked">
+                //<span class="checkmark"></span>
+
+        //<div class="manual-amount-row">
+            //<label class="radio-container radio-container-manual-amount">            
+                //<input id="manualAmountRadio" name="amount" type="radio" value="">
+                //<span class="checkmark"></span>
+            //<div class="manual-amount">
+                //<input class="text-input has-error" name="amount" id="manualAmountInput" type="text" placeholder="Amount" />    
+                //<div class="">Small text under the amount input</div>
+        //<div class="error">Amount out of range</div>    
+            
+}
+
+function getMethodData(method) {
+    return mathodData = {
+        method: method.getAttribute('data-method'),
+        selected: method.getAttribute('data-selected'),
+        amount: method.getAttribute('data-amount'),
+        predefinedamounts: method.getAttribute('data-predefinedamounts'),
+        defaultamount: method.getAttribute('data-defaultamount'),
+        currency: method.getAttribute('data-currency'),
+        custompaymentmethod: method.getAttribute('data-custompaymentmethod'),
+        errors: method.getAttribute('data-errors'),
+        customfield: method.getAttribute('data-customfield'),
+        logo: method.getAttribute('data-logo'),
+        customFieldValue: method.getAttribute('data-customfieldvalue'),
+        amountrange: method.getAttribute('data-amountrange')
+    }
+}
